feb4d263f620241bb6bddb768935994d7a6f276f
ukey-ino <bogie.pop.tere.lune@gmail.com>
2019-06-02 18:49:40 +0900

    fixed headers - remove About link - change Index.vue to ex-Blog.vue - remove Blog.vue - change site name

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'i-ukey Blog',
   plugins: [
 	  {
       use: '@gridsome/source-contentful',
