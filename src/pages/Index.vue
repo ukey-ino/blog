1d7a5f2285ca265bbfb6cabef5f9cb1c091f89bd
i-ukey <boogie.pop.terre.lune@gmail.com>
2019-06-09 19:47:34 +0900

    Enable to blog post with cover image - However, I should reference local file when blog post with no image

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,7 @@
     <h1>コンテンツ一覧</h1>
     <ul v-for="edge in $page.allContentfulBlog.edges" :key="edge.id">
       <li>
+        <g-image :src=coverUrl(edge.node.cover) />
         <p>title: {{ edge.node.title }}</p>
         <p>slug: {{ edge.node.slug }}</p>
         <p>path:
@@ -15,6 +16,22 @@
     </ul>
   </Layout>
 </template>
+
+<script>
+  export default {
+    methods: {
+      coverUrl: function (coverContent) {
+        if (coverContent) {
+          // TODO: resized by fixed width now.
+          return "https:" + coverContent.file.url + "?w=200";
+        }
+        // TODO: reference local file
+        return "https://placehold.it/150x150.png";
+      }
+    }
+  }
+</script>
+
 
 <page-query>
 query Blog ( $page: Int) {
@@ -31,6 +48,11 @@ query Blog ( $page: Int) {
         title
         slug
         content
+        cover {
+          file {
+            url
+          }
+        }
         date( format: "YYYY.MM.DD", locale: "ja" )
       }
     }
--- src/templates/ContentfulBlog.vue
+++ src/templates/ContentfulBlog.vue
@@ -2,6 +2,7 @@
   <Layout>
     <h1>投稿詳細</h1>
     <div>
+      <g-image :src="coverUrl($page.contentfulBlog.cover)" />
       <p>title: {{ $page.contentfulBlog.title }} </p>
       <p>slug: {{ $page.contentfulBlog.slug }}</p>
       <g-link :to="$page.contentfulBlog.path">
@@ -13,6 +14,21 @@
   </Layout>
 </template>
 
+<script>
+  export default {
+    methods: {
+      coverUrl: function (coverContent) {
+        if (coverContent) {
+          // TODO: resized by fixed width now.
+          return "https:" + coverContent.file.url + "?w=200";
+        }
+        // TODO: reference local file
+        return "https://placehold.it/150x150.png";
+      }
+    }
+  }
+</script>
+
 <page-query>
   query Blog( $path: String! ) {
     contentfulBlog( path: $path ) {
@@ -21,6 +37,11 @@
       title
       slug
       content
+      cover {
+        file {
+          url
+        }
+      }
       date(format: "YYYY.MM.DD", locale: "ja")
     }
   }
